0a7f8caab6a1f8feb4779311103925c9af78ae2a
Yasaman-A <yamannejad@mtroyal.ca>
2020-10-08 22:31:46 -0600

    Game component and the functionality of the code is completed

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,21 +1,80 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 import Team from './Team';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  rowContainer: {
+    display: 'flex',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  colContainer: {
+    display: 'flex',
     backgroundColor: '#fff',
     alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
   },
 });
 
-const Game = () => (
-  <View style={styles.container}>
-    <Team name="Cougar" url="https://mrucougars.com/images/logos/site/site.png" />
-    <Team name="Dinos" url="https://content.sportslogos.net/logos/77/2379/full/7465_calgary_dinos-partial-2013.png" />
-  </View>
-);
+const Game = () => {
+  const [team1Score, setTeam1Score] = useState(0);
+  const [team2Score, setTeam2Score] = useState(0);
+
+  return (
+    <View style={styles.colContainer}>
+      <Button
+        mode="contained"
+        onPress={() => {
+          setTeam1Score(0);
+          setTeam2Score(0);
+        }}
+      >
+        New Game
+      </Button>
+      <View style={styles.rowContainer}>
+        <View style={{ margin: 15 }}>
+          <Team name="Cougars" url="https://mrucougars.com/images/logos/site/site.png" />
+          <Button
+            mode="outlined"
+            onPress={() => {
+              const currentScore = team1Score;
+              setTeam1Score(currentScore + 1);
+            }}
+          >
+            Score
+          </Button>
+        </View>
+
+        <View>
+          <Team name="Dinos" url="https://content.sportslogos.net/logos/77/2379/full/7465_calgary_dinos-partial-2013.png" />
+          <Button
+            mode="outlined"
+            onPress={() => {
+              const currentScore = team2Score;
+              setTeam2Score(currentScore + 1);
+            }}
+          >
+            Score
+          </Button>
+        </View>
+      </View>
+      <View>
+        <Text>
+          {team1Score}
+          {' '}
+          vs.
+          {' '}
+          {team2Score}
+        </Text>
+      </View>
+
+    </View>
+  );
+};
 
 export default Game;
